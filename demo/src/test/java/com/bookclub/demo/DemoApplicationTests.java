8dfa99da23cbf49a5ab1ef2b5848f42b81ed292f
Kobe Alexander <koalexander@my365.bellevue.edu>
2024-03-24 22:28:37 -0500

    Added week 2 assignment

--- demo/src/test/java/com/bookclub/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/bookclub/demo/DemoApplicationTests.java
@@ -1,13 +1,1 @@
-package com.bookclub.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
