bcb48239b3acdb74a8282ebe2ca55715f5ba5444
Kobe Alexander <koalexander@my365.bellevue.edu>
2024-03-31 22:47:45 -0500

    Added week 3 assignment

---
+++ demo/src/main/java/com/bookclub/service/GenericDao.java
@@ -0,0 +1,8 @@
+package main.java.com.bookclub.service;
+
+import java.util.List;
+
+public interface GenericDao<E, K> {
+    List<E> list();
+    E find(K key);
+}
---
+++ demo/src/main/java/com/bookclub/service/dao/BookDao.java
@@ -0,0 +1,8 @@
+package main.java.com.bookclub.service.dao;
+
+import main.java.com.bookclub.model.Book;
+import main.java.com.bookclub.service.GenericDao;
+
+
+public interface BookDao extends GenericDao<Book, String> {
+}
